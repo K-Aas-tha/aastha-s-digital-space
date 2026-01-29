a8c3e8202f1374d8b8245252f04a1f256dc2f25b
K-Aas-tha <aasthanayak161@gmail.com>
2026-01-29 17:46:40 +0530

    Configue Github Pages deployment

--- vite.config.ts
+++ vite.config.ts
@@ -13,6 +13,7 @@ export default defineConfig(({ mode }) => ({
     },
   },
   plugins: [react(), mode === "development" && componentTagger()].filter(Boolean),
+  base: "/aastha-s-digital-space/",
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
